bd2557067e507eb5a9baf685bc6258ef97be5f0a
PinheiroProgramadorPython <pinheiroprogramadorpython@gmail.com>
2025-10-22 23:42:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "*",
+    origin: "https://pinheirotv.onrender.com",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ["Content-Type"]
 }));
