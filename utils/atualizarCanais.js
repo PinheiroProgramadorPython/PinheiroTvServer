5f22a7703b2f4cf5a64780e36521b76f023a95f9
PinheiroProgramadorPython <pinheiroprogramadorpython@gmail.com>
2025-10-22 23:58:06 -0300

    Update atualizarCanais.js

--- utils/atualizarCanais.js
+++ utils/atualizarCanais.js
@@ -6,31 +6,37 @@ let lista = [];
 let canais = [];
 
 let download = async () => {
-    let req = await fetch(linkPlaylist);
-    let resp = await req.text();
-    lista = resp.split("\n");
-    lista.forEach((linha, i) => {
-        if (linha.includes("http://")) {
-            lista[i] = linha.replace(".ts", ".m3u8").trim();
-        } else {
-            lista[i] = linha.replace("#EXTINF:-1,", "").trim();
-        }
-    });
+    try {
+        let req = await fetch(linkPlaylist);
+        let resp = await req.text();
+        lista = resp.split("\n");
+        lista.forEach((linha, i) => {
+            if (linha.includes("http://")) {
+                lista[i] = linha.replace(".ts", ".m3u8").trim();
+            } else {
+                lista[i] = linha.replace("#EXTINF:-1,", "").trim();
+            }
+        });
 
-    lista.forEach((linha, i) => {
-        if (linha.includes("http://")) {
-            let nome = lista[i - 1];
-            let url = lista[i];
-            canais.push({ nome: nome, url: url })
-        }
-    });
+        lista.forEach((linha, i) => {
+            if (linha.includes("http://")) {
+                let nome = lista[i - 1];
+                let url = lista[i];
+                canais.push({ nome: nome, url: url })
+            }
+        });
 
-    // await canalSchema.deleteMany({});
-    await canalSchema.insertMany(canais);
+        await canalSchema.deleteMany({});
+        await canalSchema.insertMany(canais);
 
-    console.log(`✅ ${canais.length} canais atualizados!`);
+        console.log(`✅ ${canais.length} canais atualizados!`);
 
-    return canais;
+        return canais;
+
+    } catch (error) {
+        console.error(error);
+    }
+
 };
 
 export default async function atualizarCanais() {
