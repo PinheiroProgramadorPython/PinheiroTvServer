7b8118b3aa1cf210a3195d9232f387038beb6157
PinheiroProgramadorPython <pinheiroprogramadorpython@gmail.com>
2025-10-26 22:53:46 -0300

    Update playlist.js

--- routes/playlist.js
+++ routes/playlist.js
@@ -9,8 +9,9 @@ router.get("/canais", async (req, resp) => {
         resp.writeHead(200, { "Content-Type": "application/json" });
         let canais = canalSchema.find().batchSize(1000).cursor();
         let first = true;
+
         for await (let canal of canais) {
-            canal = JSON.stringify(canal + "/n");
+            canal = JSON.stringify(canal + "\n");
             resp.write(canal);
         }
 
